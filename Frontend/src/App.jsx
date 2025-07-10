3d40b1537451ce55849997dab04fb1ea0b652579
sujoy3-bit <sujoymaitysm03@gmail.com>
2025-07-10 15:30:20 +0530

    Initial commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+       <h1 className="text-3xl font-bold underline bg-red-500 m-4" >
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
